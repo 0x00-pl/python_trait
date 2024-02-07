977768acdebbdb32ba6f4275b51996abf0dd0ce9
pl <0x00.pl@gmail.com>
2024-02-07 20:34:44 +0800

    update api

--- python_trait/__init__.py
+++ python_trait/__init__.py
@@ -1,22 +1,35 @@
 import typing
 
+trait_impl_pool = {}
+
 T = typing.TypeVar('T')
 
 
 class Trait(typing.Generic[T]):
-    def get_trait(self, obj) -> T:
+    def trait_for(self, obj) -> T:
         impl = trait_impl_pool[type(obj), type(self)]
         return impl(obj)
 
+    @classmethod
+    def cls_trait_for(cls, obj) -> T:
+        impl = trait_impl_pool[type(obj), cls]
+        return impl(obj)
+
     def __call__(self, obj) -> T:
-        return self.get_trait(obj)
-
-
-trait_impl_pool = {}
+        return self.trait_for(obj)
 
 
 def register_trait(origin_cls, trait_cls):
     def impl(trait_impl):
+        trait_impl_pool[origin_cls, trait_cls] = trait_impl
+        return trait_impl
+
+    return impl
+
+
+def trait_to(origin_cls):
+    def impl(trait_impl):
+        trait_cls = [t for t in trait_impl.__bases__ if issubclass(t, Trait)][0]
         trait_impl_pool[origin_cls, trait_cls] = trait_impl
         return trait_impl
 
--- tests/test_trait.py
+++ tests/test_trait.py
@@ -1,6 +1,6 @@
 import math
 
-from python_trait import Trait, register_trait, trait_impl_pool
+from python_trait import Trait, register_trait, trait_impl_pool, trait_to
 
 
 class NumberPair:
@@ -9,12 +9,23 @@ class NumberPair:
         self.y = y
 
 
+class AABB:
+    def __init__(self, x_min, x_max, y_min, y_max):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+
+
 class AsLine(Trait['AsLine']):
     def length(self):
-        return 0
+        raise NotImplementedError
 
 
-@register_trait(NumberPair, AsLine)
+as_line = AsLine()
+
+
+@trait_to(NumberPair)
 class NumberPairAsLine(AsLine):
     def __init__(self, number_pair: NumberPair):
         self.number_pair = number_pair
@@ -25,12 +36,41 @@ class NumberPairAsLine(AsLine):
         return math.sqrt(x * x + y * y)
 
 
+@register_trait(AABB, AsLine)
+class AABBAsLine(AsLine):
+    def __init__(self, aabb: AABB):
+        self.aabb = aabb
+
+    def length(self):
+        x = self.aabb.x_max - self.aabb.x_min
+        y = self.aabb.y_max - self.aabb.y_min
+        return math.sqrt(x * x + y * y)
+
+
 def test_register_trait():
-    assert len(trait_impl_pool) == 1
+    assert len(trait_impl_pool) == 2
     assert trait_impl_pool[NumberPair, AsLine] == NumberPairAsLine
+    assert trait_impl_pool[AABB, AsLine] == AABBAsLine
 
 
-def test_number_pair_as_line_traits():
-    number_pair = NumberPair(1, 1)
-    length = AsLine()(number_pair).length()
-    print(length)
+def test_as_line_traits():
+    obj_list = [NumberPair(1, 1), AABB(0, 1, 0, 1)]
+    length = [AsLine.cls_trait_for(i).length() for i in obj_list]
+    for i in length:
+        assert math.isclose(i, math.sqrt(2))
+
+
+def test_as_line_traits_update_value():
+    number_pair = NumberPair(0, 0)
+    aabb = AABB(0, 0, 0, 0)
+
+    lines = [as_line(i) for i in (number_pair, aabb)]
+
+    number_pair.x = 1
+    number_pair.y = 1
+    aabb.x_max = 1
+    aabb.y_max = 1
+
+    length = [i.length() for i in lines]
+    for i in length:
+        assert math.isclose(i, math.sqrt(2))
